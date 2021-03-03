ddad19e3c1104b465fd815695ef1caf1318f5336
Nicola Rennie <nrennie35@gmail.com>
2021-03-03 15:07:22 +0000

    Remake 02032021

--- 2021/02-03-2021/02032021.R
+++ 2021/02-03-2021/02032021.R
@@ -1,64 +1,58 @@
 library(tidyverse)
-library(ggimage)
-library(cowplot)
-library(magick)
-library(ggforce)
 
 tuesdata <- tidytuesdayR::tt_load('2021-03-02')
 youtube <- tuesdata$youtube
 
-youtube2020 <- filter(youtube, year == 2020)
+#function for percentages
+round_percent <- function(x) { 
+  x <- x/sum(x)*100  # Standardize result
+  res <- floor(x)    # Find integer bits
+  rsum <- sum(res)   # Find out how much we are missing
+  if(rsum<100) { 
+    # Distribute points based on remainders and a random tie breaker
+    o <- order(x%%1, sample(length(x)), decreasing=TRUE) 
+    res[o[1:(100-rsum)]] <- res[o[1:(100-rsum)]]+1
+  } 
+  res 
+}
 
-#find coordinate points of circle
-z <- 0:8
-r <- 1
-x <- r*cos(40*z + 20)
-y <- r*sin(40*z + 20)
-#plot logos as points at x and y
-d <- data.frame(x = x,
-                y = y,
-                image = c("logo/doritos.png",
-                          "logo/budweiser.png",
-                          "logo/pepsi.png",
-                          "logo/toyota.png",
-                          "logo/nfl.png",
-                          "logo/cola.png",
-                          "logo/budlight.png",
-                          "logo/kia.png",
-                          "logo/hyundai.png")
+#aggregate by brand
+ads <- youtube %>%
+  group_by(brand) %>%
+  summarise(funny = sum(funny), patriotic = sum(patriotic), celebrity=sum(celebrity), 
+            danger=sum(danger), animals=sum(animals), use_sex=sum(use_sex))
+
+num_boxes <- data.frame(cbind(brand=ads$brand, apply(ads[,2:7], 2, function(x) round_percent(x/sum(x)))))
+brand_boxes <- data.frame(apply(num_boxes[,2:7],2, function(x) rep(ads$brand, x)))
+long_data <- gather(brand_boxes, key="measure", value="value", c("funny", "patriotic", "celebrity", "danger", "animals", "use_sex"))
+long_data$x <- rep(1:10,10)
+long_data$y <- rep(1:10,rep(10,10))
+
+category_names <- list(
+  'funny'="Funny",
+  'patriotic'="Patriotic",
+  'danger'="Danger",
+  'use_sex'="Sex",
+  'celebrity'="Celebrity",
+  'animals'="Animals"
 )
-d$y[2] <- 0.05
-d$x[2] <- -0.85
-d$y[3] <- -0.65
-d$x[3] <- 0.6
-d$y[5] <- -0.65
-d$y[6] <- -0.2
-d$y[7] <- 0.64
-d$x[6] <- 0.75
-p1 <- ggplot(d, aes(x, y)) + 
-  xlim(-1.2, 1.2) + ylim(-2.1, 1.8) +
-  geom_point(data=data.frame(p=0, q=0), aes(x=p, y=q),colour="purple4", size = 145, alpha=.2) +
-  geom_ellipse(aes(x0 = 0.15, y0 = 0.4, a = 1.15, b = 0.70, angle = (140*pi / 180)), fill="springgreen4", colour="springgreen4", alpha=0.02) +
-  geom_ellipse(aes(x0 = -0.75, y0 = -0.4, a = 0.8, b = 0.4, angle = (105*pi / 180)), fill="deeppink3", colour="deeppink3", alpha=0.02) +
-  geom_ellipse(aes(x0 = -0.28, y0 = 0.58, a = 1.05, b = 0.57, angle = (37*pi / 180)), fill="dodgerblue2", colour="dodgerblue2", alpha=0.02) +
-  geom_ellipse(aes(x0 = 0.35, y0 = 0.55, a = 1.10, b = 0.60, angle = (120*pi / 180)), fill="#FFA500", colour="#FFA500", alpha=0.02) +
-  geom_image(aes(image=image),size=c(0.2,0.08,0.2,0.2,0.2,0.15,0.1,0.1,0.1), by="height") +
-  #legend
-  geom_point(data=data.frame(p=-1.0, q=-1.9), aes(x=p, y=q),colour="purple4", size = 9, alpha=.2) +
-  geom_point(data=data.frame(p=-0.5, q=-1.9), aes(x=p, y=q),colour="springgreen4", size = 9, alpha=.2) +
-  geom_point(data=data.frame(p=0, q=-1.9), aes(x=p, y=q),colour="deeppink3", size = 9, alpha=.2) +
-  geom_point(data=data.frame(p=0.5, q=-1.9), aes(x=p, y=q),colour="dodgerblue2", size = 9, alpha=.2) +
-  geom_point(data=data.frame(p=1.0, q=-1.9), aes(x=p, y=q),colour="#FFA500", size = 9, alpha=.2) +
-  #add labels
-  annotate("text", x = -1.0, y = -2.1, label = "Celebrity", hjust = 0.5, family="Berlin Sans FB") +
-  annotate("text", x = -0.5, y = -2.1, label = "Funny", hjust = 0.5, family="Berlin Sans FB") +
-  annotate("text", x = 0, y = -2.1, label = "Patriotic", hjust = 0.5, family="Berlin Sans FB") +
-  annotate("text", x = 0.5, y = -2.1, label = "Danger", hjust = 0.5, family="Berlin Sans FB") +
-  annotate("text", x = 1.0, y = -2.1, label = "Animals", hjust = 0.5, family="Berlin Sans FB") +
+
+category_labeller <- function(variable,value){
+  return(category_names[value])
+}
+
+p <- ggplot(long_data, aes(x, y, width=.7, height=.5)) +
+  geom_tile(aes(fill = value), colour = "grey85") + 
+  facet_wrap(~measure, labeller=category_labeller) +
+  labs(title="Superbowl Commercials", subtitle="Data on the 233 commercials from the 10 brands that aired the most spots in all\n21 Super Bowls this century. The boxes represent the percentage of each category\nof commercial which was made by each brand.", caption="N. Rennie | Data source: FiveThirtyEight") +
+  scale_fill_manual("", values=c('#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a')) +
   theme(panel.background = element_rect(fill = "grey85"),
         plot.background = element_rect(fill = "grey85"),
         legend.background = element_rect(fill = "grey85"),
-        legend.position="none",
+        legend.position="right",
+        plot.title = element_text(size = 14, face=2),
+        plot.subtitle = element_text(size = 10),
+        plot.caption = element_text(size = 8, hjust=0),
         plot.margin = unit(c(0.5, 0.5, 0.5, 0.5), "cm"), #top, right, bottom, left
         axis.title.x= element_blank(),
         axis.title.y= element_blank(),
@@ -72,69 +66,12 @@ p1 <- ggplot(d, aes(x, y)) +
         panel.grid.minor = element_blank()
   )
 
+p
 
-p1
-
-
-############################################# BAR CHART OF LLIKES / DISLIKES ####################################################
-
-youtube2020likes <- youtube2020 %>% select(brand, like_count, dislike_count)
-youtube2020likes <- pivot_longer(youtube2020likes, cols=c(like_count, dislike_count), values_to = "count")
-
-p2 <- ggplot(data=youtube2020likes, aes(x=brand, y=count, fill=name)) +
-  labs(y="Numbee of YouTube Likes / Dislikes\n", x="") + 
-  geom_bar(width=0.5, stat="identity", position=position_dodge(width=0.5)) +
-  scale_fill_manual("", values=c("dislike_count"="red3", "like_count"="green4"), labels = c("Dislikes", "Likes")) +
-  scale_y_continuous(trans='sqrt', breaks=c(5000,20000,50000,100000,175000), limits=c(0,176000), labels = scales::label_number_si()) +
-  coord_cartesian(expand=F) +
-  theme(panel.background = element_rect(fill = "grey85"),
-        plot.background = element_rect(fill = "grey85"),
-        legend.background = element_rect(fill = "grey85"),
-        plot.title = element_text(colour = "red3", size=9, face="bold", hjust = 0.5, family="Berlin Sans FB"),
-        plot.subtitle = element_text(colour = "red3", size=9, hjust = 0.5, family="Berlin Sans FB"),
-        legend.title = element_blank(),
-        legend.position="top",
-        legend.key = element_rect(size = 1.2, colour = "grey85"),
-        legend.spacing.x = unit(0.5,"cm"),
-        plot.margin = unit(c(0.5, 0.8, 0.8, 0.5), "cm"), #top, right, bottom, left
-        legend.text = element_text(colour="black", size=9, family="Berlin Sans FB"),
-        axis.title.y= element_text(colour="black", size=8, family="Berlin Sans FB"),
-        axis.text.x=element_blank(),
-        axis.text.y=element_text(colour = "black", size=8, family="Berlin Sans FB"),
-        axis.line = element_line(),
-        axis.ticks.x = element_blank(),
-        panel.grid.major.x = element_blank() ,
-        panel.grid.major.y = element_line(size=.1, color="black"), 
-        panel.grid.minor = element_blank()
-  )
-p2
-
-#dev.new(width=8,height=3,unit="in", noRStudioGD = TRUE)
-
-p_b <- ggdraw() +
-  draw_plot(p2) +
-  draw_image("logo/budlight.png", scale = 0.15, x=-0.33, y=-0.42) +
-  draw_image("logo/budweiser.png", scale = 0.1, x=-0.23, y=-0.42) +
-  draw_image("logo/cola.png", scale = 0.13, x=-0.14, y=-0.42) +
-  draw_image("logo/doritos.png", scale = 0.2, x=-0.05, y=-0.42) +
-  draw_image("logo/hyundai.png", scale = 0.12, x=0.05, y=-0.42) +
-  draw_image("logo/kia.png", scale = 0.12, x=0.14, y=-0.42) +
-  draw_image("logo/nfl.png", scale = 0.15, x=0.25, y=-0.42) +
-  draw_image("logo/pepsi.png", scale = 0.15, x=0.34, y=-0.42) +
-  draw_image("logo/toyota.png", scale = 0.17, x=0.43, y=-0.42)
-p_b
+ggsave(p, filename = "02032021.jpg")
 
 
-###############################################################################################################################
 
 
-p <- plot_grid(p1, p_b, ncol=1, rel_heights = c(9, 3))
 
-q <- ggdraw(p) +
-  draw_label("2020 Superbowl Commercials", x = 0.5, y = 0.95, hjust = 0.5, vjust = 0, fontfamily = "Berlin Sans FB", color = "red", size = 14) +
-  draw_label("N. Rennie | Data source: FiveThirtyEight", x = 0.15, y = 0.98, hjust = 0.5, vjust = 0, fontfamily = "Berlin Sans FB", color = "black", size = 6) 
-q
-
-
-ggsave(q, filename = "02032021.jpg")
 
--- 2021/02-03-2021/README.md
+++ 2021/02-03-2021/README.md
@@ -2,7 +2,7 @@
 2020 Superbowl Commercials
 </h1>
 
-The data this week comes from FiveThirtyEight. They watched 233 commercials from the 10 brands that aired the most spots in all 21 Super Bowls this century, according to superbowl-ads.com. They evaluated commercials using seven specific criteria, marking every spot as a "yes" or "no" for each. All commercials featured a celebrity. The NFL commercial received far more likes on YouTube than any other Superbowl commercial in 2020.
+The data this week comes from FiveThirtyEight. They watched 233 commercials from the 10 brands that aired the most spots in all 21 Super Bowls this century, according to superbowl-ads.com. They evaluated commercials using specific criteria, marking every spot as a "yes" or "no" for each. The boxes represent the percentage of each category of commercial which was made by each brand.
 
 <p align="center">
 <img src="https://github.com/nrennie/tidytuesday/blob/main/2021/02-03-2021/02032021.jpg?raw=true" width="50%">
