98a2e859db9f6cc24b7d3cff9c983b7c61112567
Nicola Rennie <nrennie35@gmail.com>
2021-09-23 13:10:32 +0100

    add version with waffle package

--- 2021/21-09-2021/21092021.R
+++ 2021/21-09-2021/21092021.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(extrafont)
 library(patchwork)
 library(cowplot)
-library(ggalluvial)
+library(waffle)
 
 tuesdata <- tidytuesdayR::tt_load('2021-09-21')
 nominees <- tuesdata$nominees
@@ -151,3 +151,52 @@ q <- ggdraw() +
   draw_plot(p) +
   draw_image(logo, x = 0.5, y = 0.98, hjust = 0.5, vjust = 1, halign = 1, valign = 1, width = 0.3)
 q
+
+
+########################################## WAFFLE PLOT ##################################################
+plot_data$title <- factor(plot_data$title, 
+                          levels=c("The Amazing Race", "Game Of Thrones", "Dancing With The Stars", "The Voice", "Saturday Night Live"), 
+                          labels=c("The\nAmazing\nRace", "Game\nOf\nThrones", "Dancing\nWith\nThe Stars", "The\nVoice\n", "Saturday\nNight\nLive"))
+
+p <- ggplot(plot_data, aes(fill = type, values = n)) +
+  geom_waffle(color = "grey15", size = .5, n_rows = 15, flip = TRUE) +
+  facet_wrap(~title, nrow = 1, strip.position = "top") +
+  scale_x_discrete() + 
+  scale_y_continuous(labels = function(x) x * 15, # make this multiplyer the same as n_rows
+                     expand = c(0,0)) +
+  scale_fill_manual("", values=c("Nominee"="#f6d271", "Winner"="#f1b718")) +  
+  coord_equal() +
+  labs(
+    x="",
+    y="",
+    caption="N.Rennie | Data: emmys.com\n", 
+    subtitle="\n\n\n\n\n\n\n\nAlthough Game of Thrones has only won the fourth most Emmy awards,\nit has won more than half of the awards it has been nominated for."
+  ) +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  theme(panel.background = element_rect(fill = "grey15", colour="grey15"),
+        plot.background = element_rect(fill = "grey15", colour="grey15"),
+        strip.background =element_rect(fill=alpha("grey15",0.2)),
+        strip.text = element_text(colour = '#f6d271', family="Ebrima", size=12),
+        legend.background = element_rect(fill = "grey15"),
+        legend.key = element_rect(fill = "grey15", colour="grey15"), 
+        legend.text =  element_text(colour = "#f1b718", size=16, hjust = 0.5, family="Ebrima"),
+        plot.subtitle = element_text(colour = "#f1b718", size=16, hjust = 0.5, family="Ebrima"),
+        plot.title = element_text(colour = "#f1b718", size=20, face="bold", hjust = 0.5, family="Ebrima"),
+        legend.position="bottom",
+        plot.margin = unit(c(0.3, 0.3, 0.3, 0.3), "cm"), #top, right, bottom, left
+        axis.title= element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.spacing = unit(2.5, "lines"))
+p
+
+#add logos
+logo <- "./images/emmy.png"
+q <- ggdraw(p) +
+  theme(panel.background = element_rect(fill = "grey15", colour = "grey15")) +
+  draw_image(logo, x = 0.5, y = 0.98, hjust = 0.5, vjust = 1, halign = 1, valign = 1, width = 0.3)
+q
+
+
