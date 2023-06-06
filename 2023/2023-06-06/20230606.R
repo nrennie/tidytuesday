8b3410eac1e0f5f0a80b45bebc1145b91aa0bdd7
nrennie <nrennie35@gmail.com>
2023-06-06 17:47:54 +0100

    add second version of plot

--- 2023/2023-06-06/20230606.R
+++ 2023/2023-06-06/20230606.R
@@ -158,3 +158,100 @@ gg_playback(
   frame_duration = .25,
   background = bg_col
 )
+
+
+# No Indonesia ------------------------------------------------------------
+
+st <- "Global demand for coal is at an all-time high, but looking at the
+changes in coal production per capita show that the story of coal production
+is very different across the globe. In the last 20 years, Venezuela has 
+drastically decreased coal production."
+highlight_country <- "Venezuela"
+
+# plot
+ggplot(filter(plot_data, country != "Indonesia")) +
+  geom_line(
+    mapping = aes(
+      x = year, y = coal_prod_per_capita, group = country
+    ),
+    colour = light_col,
+    linewidth = 0.4
+  ) +
+  geom_line(
+    data = filter(plot_data, country == highlight_country),
+    mapping = aes(
+      x = year, y = coal_prod_per_capita, group = country
+    ),
+    colour = highlight_col,
+    linewidth = 0.4
+  ) +
+  geom_segment(
+    mapping = aes(x = year, xend = year,
+                  y = -100, yend = 10000)
+  ) +
+  geom_text(
+    data = data.frame(
+      x = 2015,
+      y = pull(filter(plot_data, country == highlight_country, year == 2020), coal_prod_per_capita),
+      label = highlight_country),
+    mapping = aes(x = x, y = y, label = label),
+    colour = highlight_col,
+    family = "Roboto Slab",
+    size = 7
+  ) +
+  labs(
+    title = title,
+    subtitle = st,
+    caption = cap,
+    x = "",
+    y = "% change per capita since 1960"
+  ) +
+  scale_x_continuous(breaks = c(1960, 1980, 2000, 2020),
+                     limits = c(1959.9, 2020.1)) +
+  coord_cartesian(expand = FALSE) +
+  theme_minimal(
+    base_size = 30,
+    base_family = "Roboto"
+  ) +
+  theme(
+    legend.position = "none",
+    plot.background = element_rect(
+      fill = bg_col,
+      colour = bg_col
+    ),
+    panel.background = element_rect(
+      fill = bg_col,
+      colour = bg_col
+    ),
+    plot.title = element_textbox_simple(
+      family = "Roboto Slab",
+      face = "bold",
+      margin = margin(t = 10, b = 15),
+      colour = dark_col,
+      hjust = 0,
+      halign = 0,
+      lineheight = 0.4,
+    ),
+    plot.subtitle = element_textbox_simple(
+      lineheight = 0.5,
+      colour = dark_col,
+      margin = margin(b = 20),
+      halign = 0
+    ),
+    plot.caption = element_textbox_simple(
+      lineheight = 0.55,
+      colour = dark_col,
+      hjust = 0,
+      halign = 0
+    ),
+    axis.text = element_text(family = "Roboto Slab"),
+    axis.title = element_text(size = 30),
+    panel.grid.major.x = element_blank(),
+    panel.grid.major.y = element_line(
+      colour = alpha(dark_col, 0.5),
+      linewidth = 0.5,
+    ),
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.margin = margin(5, 15, 5, 10),
+  )
