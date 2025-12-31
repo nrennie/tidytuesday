42e7eac3868bc5e64e72085076d074f5522d6ae3
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2025-12-31 10:29:19 +0000

    update data script for 2026

--- data/make-data.R
+++ data/make-data.R
@@ -41,7 +41,7 @@ all_weeks <- all_folders |>
     too_few = "align_start",
     too_many = "drop"
   ) |>
-  filter(year %in% c(2019, 2020, 2021, 2022, 2023, 2024, 2025)) |>
+  filter(year %in% c(2021, 2022, 2023, 2024, 2025, 2026)) |>
   drop_na(week) |>
   mutate(
     title = NA_character_,
