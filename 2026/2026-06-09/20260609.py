dadbbf3b897dabe5c259c2efb5da2be1b88b7aae
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-06-02 16:50:06 +0100

    Add interactive version

--- 2026/2026-06-09/20260609.py
+++ 2026/2026-06-09/20260609.py
@@ -5,6 +5,7 @@ import matplotlib.font_manager as fm
 import os
 import highlight_text as ht
 import re
+from ninejs import interactive, css, save, show
 
 
 # Load data ---------------------------------------------------------------
@@ -126,13 +127,21 @@ st = "Percentage of films released each period rated A-F (%)"
 cap = '<Note::{"fontweight": "bold"}>: Data excludes films where either the release date or CinemaScore rating are unknown. \n<Data::{"fontweight": "bold"}>: Wikipedia (List of films based on video games)\n<Graphic::{"fontweight": "bold"}>: Nicola Rennie (@nrennie)'
 desc_st = "Many video games have been adapted into films. Earlier video game movies tended to\nbe either disasters or cult hits, with all of the films with the <lowest rating (F)>\nfrom CinemaScore released before 2005. In recent years, video game inspired films\nare again becoming popular, with the <highest rating (A)> films released in the\nearly 2020s."
 
+plot_data['summary'] = (
+    plot_data['year_group'].astype(str)
+    + ': '
+    + plot_data['pct'].round().astype(int).astype(str)
+    + '% rated '
+    + plot_data['cinema_score'].astype(str)
+)
+
 
 # Plot --------------------------------------------------------------------
 
 p = (
     gg.ggplot(
         plot_data,
-        gg.aes(x="year_group", y="pct", fill="cinema_score", group="cinema_score")
+        gg.aes(x="year_group", y="pct", fill="cinema_score", group="cinema_score",)
     )
     + gg.geom_col(color=bg_col, size=3)
     + gg.scale_fill_manual(
@@ -213,3 +222,106 @@ ht.fig_text(
 
 fpath = os.path.join("2026", "2026-06-09", f'{"20260609"}.png')
 fig.savefig(fpath, dpi=300)
+
+
+# Interactive -------------------------------------------------------------
+
+# Mostly works out of the box with original 'p' but 
+# needs to tweak position of text
+
+p = (
+    gg.ggplot(
+        plot_data,
+        gg.aes(x="year_group", y="pct", fill="cinema_score", group="cinema_score", tooltip="summary", data_id="year_group")
+    )
+    + gg.geom_col(color=bg_col, size=3)
+    + gg.scale_fill_manual(
+    values={
+        "A": "#07fdf5",
+        "B": "#5C5C5C",
+        "C": "#474747",
+        "D": "#333333",
+        "F": "#f2020d",
+    }
+  )
+  + gg.labs(
+        title=title,
+        subtitle=st,
+        caption=" \n\n ",
+    )
+  + gg.coord_cartesian(expand=False)
+  + gg.theme_minimal(base_size=10, base_family=body_font)
+  + gg.theme(
+        plot_background=gg.element_rect(fill=bg_col, color=bg_col),
+        panel_background=gg.element_rect(fill=bg_col, color=bg_col),
+        plot_title=gg.element_text(
+            color=text_col,
+            ha="left",
+            weight="bold",
+            size=20,
+            margin={"b": 50, "t": 5},
+            family=title_font,
+        ),
+        plot_subtitle=gg.element_text(
+            color=text_col,
+            ha="left",
+            size=8.5,
+            family=body_font,
+        ),
+        plot_caption=gg.element_text(
+            margin={"b": 15},
+        ),
+        legend_position="none",
+        axis_title_x=gg.element_blank(),
+        axis_title_y=gg.element_blank(),
+        plot_title_position="plot",
+        axis_text_x=gg.element_text(size=7.5, lineheight=1, color=text_col),
+        axis_text_y=gg.element_text(color=text_col),
+        panel_grid_minor=gg.element_blank(),
+        panel_grid_major=gg.element_blank()
+    )
+)
+
+fig = p.draw()
+fig.set_size_inches(7, 7, forward=True)
+
+plt.figure(fig)
+
+ht.fig_text(
+    x=0.01, y=0.10, s=cap,
+    fontsize=7.5, va='top',
+    fontfamily=body_font,
+    color=text_col,
+    linespacing=0.3,
+    fig=fig)
+  
+ht.fig_text(
+    x=0.01, y=0.92,
+    s=desc_st,
+    highlight_textprops=[
+        {"color": "#f2020d", "fontweight": "bold"},
+        {"color": "#07fdf5", "fontweight": "bold"}
+    ],
+    size = 9,
+    fontfamily=body_font,
+    color=text_col,
+    linespacing=0.3,
+    fig=fig,
+)
+
+(
+    interactive(p, hover_nearest=True)
+    + css(from_dict={".tooltip": {"font-size": "2em"}})
+    + css(from_file="2026/2026-06-09/style.css")
+    + show()
+)
+
+# For some reason, it draws subtitle text twice
+# Use CSS to hide one
+(
+    interactive(p, hover_nearest=True)
+    + css(from_dict={".tooltip": {"font-size": "2em"}})
+    + css(from_file="style.css")
+    + save("2026/2026-06-09/20260609.html")
+)
+
--- 2026/2026-06-09/README.md
+++ 2026/2026-06-09/README.md
@@ -3,3 +3,7 @@
 <p align="center">
   <img src="/2026/2026-06-09/20260609.png" width="60%">
 </p>
+
+<p align="center">
+  <img src="/2026/2026-06-09/20260609.gif" width="60%">
+</p>
