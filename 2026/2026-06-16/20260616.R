5df8ad54caa99b534efbb4db9ecc443addbf6d12
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-06-16 22:05:22 +0100

    Add another chart for UK baby names

--- 2026/2026-06-16/20260616.R
+++ 2026/2026-06-16/20260616.R
@@ -1,4 +1,4 @@
-# Load packages -----------------------------------------------------------
+## Load packages -----------------------------------------------------------
 
 library(tidyverse)
 library(showtext)
@@ -8,7 +8,7 @@ library(glue)
 library(ggview)
 
 
-# Load data ---------------------------------------------------------------
+## Load data ---------------------------------------------------------------
 
 tuesdata <- tidytuesdayR::tt_load("2026-06-16")
 england_wales_names <- tuesdata$england_wales_names |>
@@ -20,7 +20,7 @@ scotland_names <- tuesdata$scotland_names |>
 all_names <- rbind(rbind(ni_names, scotland_names), england_wales_names)
 
 
-# Load fonts --------------------------------------------------------------
+## Load fonts --------------------------------------------------------------
 
 font_add_google("Oswald")
 font_add_google("Nunito")
@@ -30,7 +30,7 @@ title_font <- "Oswald"
 body_font <- "Nunito"
 
 
-# Define colours and fonts-------------------------------------------------
+## Define colours and fonts-------------------------------------------------
 
 bg_col <- "#F2F4F8"
 text_col <- "#151C28"
@@ -38,7 +38,7 @@ highlight_col <- "#7F055F"
 comp_col <- "#197176"
 
 
-# Data wrangling ----------------------------------------------------------
+## Data wrangling ----------------------------------------------------------
 
 girl_names <- all_names |>
   dplyr::filter(Sex == "Girl") |>
@@ -78,7 +78,7 @@ plot_data$Name <- factor(plot_data$Name, levels = chosen_names)
 annotate_dates$Name <- factor(annotate_dates$Name, levels = chosen_names)
 
 
-# Define text -------------------------------------------------------------
+## Define text -------------------------------------------------------------
 
 social <- nrBrand::social_caption(
   bg_colour = bg_col,
@@ -91,7 +91,7 @@ st <- glue("Trends in baby names can often be linked to popular culture, with na
 cap <- paste0("**Note**: Black vertical lines indicate year of birth for Prince George, Princess Charlotte, and Prince Louis.<br>", source_caption(source = "Office for National Statistics | Northern Ireland Statistics and Research Agency | National Records Scotland", graphic = social))
 
 
-# Plot --------------------------------------------------------------------
+## Plot --------------------------------------------------------------------
 
 ggplot(
   data = plot_data,
@@ -172,9 +172,188 @@ ggplot(
   ) -> p
 
 
-# Save --------------------------------------------------------------------
+## Save --------------------------------------------------------------------
 
 save_ggplot(
   plot = p,
   file = file.path("2026", "2026-06-16", paste0("20260616", ".png"))
 )
+
+
+
+
+# Plot 2 ------------------------------------------------------------------
+
+## Data wrangling ----------------------------------------------------------
+
+plot_data <- all_names |> 
+  select(-Rank) |> 
+  drop_na(Number) |> 
+  filter(Number >= 3) |> 
+  group_by(Name, Year, Sex) |> 
+  mutate(Number = sum(Number)) |> 
+  ungroup() |> 
+  select(-Region) |> 
+  distinct() |> 
+  pivot_wider(names_from = Sex, values_from = Number) |> 
+  drop_na(Boy, Girl) |> 
+  filter(Year >= 1997, Year <= 2024) |> 
+  group_by(Name) |> 
+  mutate(n_years = n()) |> 
+  ungroup() |> 
+  filter(n_years == 28) |> 
+  select(-n_years) |> 
+  mutate(Total = Boy + Girl,
+         Girl_p = Girl / Total,
+         Boy_p = Boy / Total) |> 
+  arrange(abs(0.5 - Girl_p)) |> 
+  group_by(Name) |> 
+  mutate(Total_all = sum(Total)) |> 
+  ungroup() |> 
+  group_by(Name) |> 
+  arrange(-Year) |> 
+  mutate(min_perc_g = Girl_p[1]) |> 
+  mutate(min_perc_b = Boy_p[1]) |> 
+  ungroup() |> 
+  filter(min_perc_g >= 0.3, min_perc_g <= 0.7) |> 
+  filter(Total_all >= 4000) |> 
+  select(-ends_with("_p")) |> 
+  select(-Total_all) |> 
+  select(-starts_with("min_perc")) |> 
+  pivot_longer(cols = c(Boy, Girl),
+               values_to = "Number",
+               names_to = "Sex")
+
+labels <- plot_data |> 
+  filter(Year == 2024, Sex == "Girl") |> 
+  mutate(perc = round(100 * Number / Total)) |> 
+  arrange(desc(perc))
+
+labels$Name <- factor(labels$Name, levels = labels$Name)
+plot_data$Name <- factor(plot_data$Name, levels = labels$Name)
+
+
+## Define text -------------------------------------------------------------
+
+social <- nrBrand::social_caption(
+  bg_colour = bg_col,
+  icon_colour = highlight_col,
+  font_colour = text_col,
+  font_family = body_font
+)
+title <- "Most popular gender neutral names"
+st <- glue("Percentage of babies born that are <span style='color:{highlight_col};'>**girls**</span> and <span style='color:{comp_col};'>**boys**</span>. 1997 to 2024.")
+cap <- paste0("**Note**: Includes names where percentage of names given to baby girls is between 30% and 70% in 2024, with at least 4,000 babies named between 1997 and 2024 across the UK.<br>", source_caption(source = "Office for National Statistics | Northern Ireland Statistics and Research Agency | National Records Scotland", graphic = social))
+
+
+## Plot --------------------------------------------------------------------
+
+ggplot() +
+  annotate("segment", x = 1997, xend = 2024, y = 0.5, yend = 0.5,
+           colour = text_col, alpha = 0.6) +
+  geom_area(
+    data = plot_data,
+    mapping = aes(x = Year, y = Number, fill = Sex),
+    position = "fill",
+    alpha = 0.5
+  ) +
+  geom_line(
+    data = plot_data,
+    mapping = aes(x = Year, y = Number, colour = Sex),
+    position = "fill"
+  ) +
+  geom_point(
+    data = labels,
+    mapping = aes(x = 2024, y = perc / 100),
+    colour = highlight_col
+  ) +
+  geom_text(
+    data = labels,
+    mapping = aes(x = 2024.5, y = perc / 100, label = paste0(" ", perc, "%")),
+    hjust = 0,
+    size = 3.4,
+    colour = highlight_col,
+    fontface = "bold"
+  ) +
+  annotate("text", x = 2023.25, y = 0.05, label = "Girls",
+           colour = highlight_col, fontface = "bold",
+           hjust = 1) +
+  annotate("text", x = 2023.25, y = 0.95, label = "Boys",
+           colour = comp_col, fontface = "bold",
+           hjust = 1, vjust = 0.5) +
+  facet_wrap(~Name, ncol = 4, axes = "all_x") +
+  scale_x_continuous(guide = guide_axis(check.overlap = TRUE)) +
+  scale_y_continuous(labels = scales::label_percent()) +
+  scale_fill_manual(values = c("Girl" = highlight_col, "Boy" = comp_col)) +
+  scale_colour_manual(values = c("Girl" = highlight_col, "Boy" = "transparent")) +
+  labs(
+    x = NULL, y = NULL,
+    title = title,
+    subtitle = st,
+    caption = cap) +
+  coord_cartesian(expand = FALSE, clip = "off") +
+  theme_minimal(base_family = body_font, base_size = 11) +
+  theme(
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    legend.position = "none",
+    plot.background = element_rect(fill = bg_col, colour = bg_col),
+    panel.background = element_rect(fill = bg_col, colour = bg_col),
+    plot.title = element_textbox_simple(
+      colour = text_col,
+      hjust = 0,
+      halign = 0,
+      margin = margin(b = 5, t = 5),
+      family = title_font,
+      face = "bold",
+      size = rel(1.5)
+    ),
+    plot.subtitle = element_textbox_simple(
+      colour = alpha(text_col, 0.7),
+      hjust = 0,
+      halign = 0,
+      margin = margin(b = 10, t = 5),
+      family = body_font
+    ),
+    plot.caption = element_textbox_simple(
+      colour = text_col,
+      hjust = 0,
+      halign = 0,
+      margin = margin(b = 0, t = 10),
+      family = body_font,
+      width = 1.063
+    ),
+    strip.text = element_textbox_simple(
+      face = "bold",
+      margin = margin(t = 0, b = 5),
+      family = title_font,
+      hjust = 0,
+      lineheight = 0.4,
+      size = rel(1)
+    ),
+    axis.title.x = element_text(
+      hjust = 1, colour = alpha(text_col, 0.7),
+      margin = margin(r = -5, t = 3)
+    ),
+    panel.spacing.y = unit(1, "lines"),
+    panel.spacing.x = unit(2, "lines"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    axis.ticks.x = element_line(linewidth = 0.4, colour = "grey50"),
+    plot.margin = margin(10, 35, 10, 10)
+  ) +
+  canvas(
+    width = 7, height = 7,
+    units = "in", bg = bg_col,
+    dpi = 300
+  ) -> p
+
+
+## Save --------------------------------------------------------------------
+
+save_ggplot(
+  plot = p,
+  file = file.path("2026", "2026-06-16", paste0("20260616_v2", ".png"))
+)
+
+
--- 2026/2026-06-16/README.md
+++ 2026/2026-06-16/README.md
@@ -3,3 +3,7 @@
 <p align="center">
   <img src="/2026/2026-06-16/20260616.png" width="60%">
 </p>
+
+<p align="center">
+  <img src="/2026/2026-06-16/20260616_v2.png" width="60%">
+</p>
